5cb290e92858498e36f21bb212a16b92b2f805fd
xiron9090 <xiron9090@gmail.com>
2023-05-17 12:22:11 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { MiPrimerComponente } from "./components/mir-primer-componente";
 import { MiSegundoComponente } from "./components/mir-segundo-componente";
 import { title } from "./mock";
+import { useState } from "react";
 
 function App() {
+  const [showTitile, setShowTitle] = useState<boolean>(false);
+  const [title1,setTitle1]=useState('')
+  useEffect(() => {
+    console.log('Renderizando')
+  }, [showTitile]);
+
+  
+  const handleshowTitle = () => {
+    setShowTitle((prevVlaue) => !prevVlaue);
+  };
   return (
     <>
+      <button onClick={handleshowTitle}>
+        {!showTitile ? `Mostrar Titulo` : "Ocultar Titulo"}
+      </button>
       {title.map((element) => (
         <Fragment key={element.id}>
-          <MiPrimerComponente title={element.title} />
-          <MiSegundoComponente title={element.title}>
-            <p>{element.title}</p>
-          </MiSegundoComponente>
+          {showTitile && <MiPrimerComponente title={element.title} />}
+          
         </Fragment>
+        
       ))}
+      <MiSegundoComponente title={title1} onclick={(e)=>setTitle1(e)}>
+            <p>{title1}</p>
+          </MiSegundoComponente>
+      <MiSegundoComponente title={title1}>
+            <p>{title1}</p>
+          </MiSegundoComponente>
     </>
   );
 }
--- src/components/mir-segundo-componente.tsx
+++ src/components/mir-segundo-componente.tsx
@@ -2,13 +2,21 @@ import { FC, ReactNode } from "react";
 
 type MiSegundoComponenteProps = {
   title: string;
-  children?:ReactNode
+  children?: ReactNode;
+  onclick?: (value: string) => void;
 };
-export const MiSegundoComponente:FC<MiSegundoComponenteProps> = ({children,title}) => {
+export const MiSegundoComponente: FC<MiSegundoComponenteProps> = ({
+  children,
+  title,
+  onclick,
+}) => {
   return (
     <>
+      <button onClick={() => onclick("Titulo cambiado desde el hijos")}>
+        Cambiar Titutlo
+      </button>
       <h1>{title}</h1>
-      <h3> hola </h3>{children}
+      {children}
     </>
   );
 };
